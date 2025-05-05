61da5afe8ab52cb3a99042a8fb00729e57cb19a9
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-05-05 09:51:54 -0700

    update version # and add mini test for boudewynSubsetTable function

---
+++ tests/testthat/test-Boudewyn_subsetTables.R
@@ -0,0 +1,23 @@
+if (!testthat::is_testing()) source(testthat::test_path("setup.R"))
+
+table3 <- reproducible::prepInputs(url = "https://nfi.nfis.org/resources/biomass_models/appendix2_table3.csv",
+                                      fun = "data.table::fread",
+                                      destinationPath = testDirs$temp$inputs,
+                                      filename2 = "appendix2_table3.csv")
+eco <- c("9")
+
+thisAdmin <- data.frame(
+  AdminBoundaryID = 9,
+  stump_parameter_id = 9,
+  adminName = "Saskatchewan",
+  abreviation = "SK",
+  SpatialUnitID = 28,
+  EcoBoundaryID = 9
+)
+
+test_that("boudewynSubsetTables", {
+  out <- boudewynSubsetTable(table = table6, thisAdmin = ThisAdmin, eco = eco)
+
+  expect_is(out, "data.table")
+  expect_named(out, "smallTable")
+})
