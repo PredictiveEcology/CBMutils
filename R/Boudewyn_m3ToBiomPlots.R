f9c5e5a67f2c9ff9aaf9f423ea57b4a459d60269
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-05-14 09:17:55 -0700

    m3ToBiomPlots bugfix and documentation update

--- R/Boudewyn_m3ToBiomPlots.R
+++ R/Boudewyn_m3ToBiomPlots.R
@@ -2,17 +2,18 @@ utils::globalVariables(c(
   "..id_col", "age", "value", "valueNumeric", "variable", "set"
 ))
 
-#' Plot all columns that are not id_col
+#' Makes plots of all columns for each id_col ID
 #'
-#' @param inc DESCRIPTION NEEDED
-#' @param id_col DESCRIPTION NEEDED
+#' @param inc increment data
+#' @param id_col column by which to group plots
+#' @param title Title of plots
 #'
 #' @export
 #' @importFrom data.table copy melt
 #' @importFrom ggforce facet_wrap_paginate
 #' @importFrom ggplot2 aes element_text geom_line ggplot labs theme theme_bw
 #' @importFrom patchwork wrap_plots plot_layout plot_annotation
-m3ToBiomPlots <- function(inc, id_col = "gcids") {
+m3ToBiomPlots <- function(inc, id_col = "gcids", title) {
   gInc <- copy(inc)
   colsToRemove <- intersect(c("id", "ecozone"), names(gInc))
   if (length(colsToRemove) > 0) {
@@ -35,14 +36,14 @@ m3ToBiomPlots <- function(inc, id_col = "gcids") {
 
   plotPages <- split(plots, ceiling(seq_along(plots) / 6))
 
-  Plots <- list()
+  plotList <- list()
 
   for (i in seq_along(plotPages)) {
-   Plots[[i]] <- wrap_plots(plotlist = plotPages[[i]], ncol = 3) +
+    plotList[[i]] <- wrap_plots(plotlist = plotPages[[i]], ncol = 3) +
      plot_layout(guides = "collect") +
-     plot_annotation(title = paste("Cumulative merch fol other by gcid - Page", i)) &
+     plot_annotation(title = paste(title, "- Page", i)) &
      theme(legend.position = "right")
   }
-  return(Plots)
+  return(plotList)
 }
 
