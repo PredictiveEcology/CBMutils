a5870384d1bd2327022805ada0f486e1787b6958
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-06-17 14:13:16 -0700

    add calcRootC

---
+++ R/CBM-tools_calcRootC.R
@@ -0,0 +1,65 @@
+
+#' `calcRootC`
+#' 
+#' `calcRootC` calculates the mass of carbon in roots pools from above ground pools
+#'
+#' @param aboveGroundC data.table with mass of carbon (tonnes/ha) in the Merch, Foliage and Other pools
+#' @param sw_hw a boolean vector indicating if the cohort is softwood (1) or hardwood (0)
+#' @param a_sw DESCRIPTION NEEDED
+#' @param b_sw DESCRIPTION NEEDED
+#' @param a_hw DESCRIPTION NEEDED
+#' @param b_hw DESCRIPTION NEEDED
+#' @param a_frp DESCRIPTION NEEDED
+#' @param b_frp DESCRIPTION NEEDED
+#'
+#' @references
+#' Li, Z., Kurz, W. A., Apps, M. J., & Beukema, S. J. (2003). Belowground biomass 
+#' dynamics in the Carbon Budget Model of the Canadian Forest Sector: recent improvements 
+#' and implications for the estimation of NPP and NEP. Canadian journal of forest 
+#' research, 33(1), 126-136.
+#'
+#' @returns data.table with mass of carbon (tonnes/ha) in coarseRoots and fineRoots pools.
+#' @export
+#'
+calcRootC <- function(aboveGroundC, sw_hw,
+                      a_sw = 0.222, b_sw = 1,
+                      a_hw = 1.576, b_hw = 0.615, 
+                      a_frp = 0.072, b_frp = 0.354){
+  
+  aboveGroundColumns <- c("Merch", "Foliage", "Other")
+  
+  if(!all(aboveGroundColumns %in% names(aboveGroundC))) {
+    stop("aboveGroundC needs the columns: ", paste(aboveGroundColumns, collapse = ", "))
+  }
+  
+  # Calculate the total above ground mass of carbon
+  totAGC <- rowSums(aboveGroundC[, ..aboveGroundColumns])
+  
+  # Convert Mg/ha of Carbon to Mg/ha of biomass
+  totAGB <- totAGC * 2
+  
+  # Calculate root total biomass
+  if(!all(sw_hw %in% c(1,0))) {
+    stop("sw_hw needs to be a boolean vector")
+  }
+  
+  rootTotBiom <- ifelse(sw_hw == 1,
+                        a_sw * totAGB^b_sw,
+                        a_hw * totAGB^b_hw)
+  
+  # Calculate the proportion of fine roots
+  fineRootProp <- a_frp + b_frp * exp(-0.060 * rootTotBiom)
+  
+  
+  # Calculate the proportion of fine roots
+  rootBiom <- data.table(
+    coarseRoots = rootTotBiom * (1 - fineRootProp),
+    fineRoots = rootTotBiom * fineRootProp
+  )
+  
+  # Reconvert into tonnes/ha of carbon
+  rootC <- rootBiom * 0.5
+  
+  return(rootC)
+  
+}
---
+++ tests/testthat/test-CBM-tools_calcRootC.R
@@ -0,0 +1,32 @@
+
+if (!testthat::is_testing()) source(testthat::test_path("setup.R"))
+
+test_that("calcRootC", {
+  ABC <- data.table(Merch = c(0,1),
+                    Foliage = c(3,1),
+                    Other = c(1,1))
+  sw_hw = c(1,0)
+  rootC <- calcRootC(aboveGroundC = ABC, sw_hw = sw_hw)
+
+  # Works correctly
+  expect_identical(
+    round(rootC,3), 
+    data.table(coarseRoots = c(0.541, 1.569),
+               fineRoots = c(0.347, 0.802))
+  )
+  expect_named(rootC, c("coarseRoots", "fineRoots"))
+  
+  # Error with missspecified inputs
+  expect_error(
+    calcRootC(aboveGroundC = ABC[,.(Merch, Foliage, other = Other)], sw_hw = sw_hw)
+  )
+  
+  expect_error(
+    calcRootC(aboveGroundC = ABC[,.(Merch, Foliage)], sw_hw = sw_hw)
+  )
+  
+  expect_error(
+    calcRootC(aboveGroundC = ABC, sw_hw = c("sw", "hw"))
+  )
+  
+})
