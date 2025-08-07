db30d8dbe6dcd5f1f4efd4ecb2d0bfcd9b3182fb
Susan Murray <32853084+suz-estella@users.noreply.github.com>
2025-08-07 11:19:48 -0700

    extractToRast and writeRasterWithValues functions added (#60)

---
+++ R/Misc-extractToRast.R
@@ -0,0 +1,145 @@
+
+#' Extract to raster
+#'
+#' Extract values from a spatial data source for each cell of a template raster.
+#' Resampling is done using "mode" resampling to preserve categorical values.
+#'
+#' @param input terra SpatRaster, one or more raster files, or sf polygons.
+#' @param templateRast SpatRaster. Raster template.
+#' @param field numeric or character. Vector field to extract.
+#'
+#' @export
+#' @return vector with a value for each cell of `templateRast`.
+#' Data type matches input data type.
+extractToRast <- function(input, templateRast, field = 1){
+
+  if (inherits(input, "sf")){
+    extractToRast_vect(input, templateRast, field = field)
+
+  }else{
+    extractToRast_rast(input, templateRast)
+  }
+}
+
+# Extract values from spatial data source: raster
+extractToRast_rast <- function(input, templateRast){
+
+  if (length(find.package("exactextractr", quiet = TRUE)) == 0) stop(
+    "exactextractr package required. Install with `install.packages(\"exactextractr\")`")
+
+  # Read as SpatRaster; mosaic tiles if need be
+  if (!inherits(input, "SpatRaster")){
+
+    if (length(input) > 1 && is.character(input) &&
+        all(tryCatch(file.exists(input), error = function(e) FALSE))){
+      input <- do.call(terra::mosaic, lapply(input, terra::rast))
+
+    }else{
+      input <- terra::rast(input)
+    }
+  }
+
+  # Keep only the first band
+  if (terra::nlyr(input) > 1) input <- input[[1]]
+
+  reproject <- !terra::compareGeom(
+    input, templateRast,
+    crs = TRUE, warncrs = FALSE, stopOnError = FALSE, messages = FALSE,
+    lyrs = FALSE, ext = FALSE, rowcol = FALSE, res = FALSE)
+
+  # Crop input
+  input <- terra::crop(
+    input,
+    terra::project(terra::as.polygons(templateRast, extent = TRUE), terra::crs(input)),
+    snap = "out")
+
+  # Reclassify if contains NAs
+  valUq <- terra::unique(input, na.rm = FALSE)
+  if (length(valUq[,1]) == 1){
+    return(rep(valUq[1,1], terra::ncell(templateRast)))
+  }
+  if (any(c(NA, NaN) %in% valUq[,1])){
+    valUq$temp <- 1:nrow(valUq)
+    input <- terra::classify(input, valUq)
+  }else valUq <- NULL
+
+  if (reproject){
+    input <- terra::project(input, templateRast, method = "mode")
+
+  }else{
+    terra::crs(input) <- terra::crs(templateRast)
+    input <- exactextractr::exact_resample(input, templateRast, fun = "mode")
+  }
+
+  # Extract and return raster values
+  alignVals <- terra::values(input, mat = FALSE)
+  if (!is.null(valUq)) alignVals <- valUq[[1]][alignVals]
+  alignVals
+
+}
+
+# Extract values from spatial data source: vector
+extractToRast_vect <- function(input, templateRast, field = 1){
+
+  if (length(find.package("exactextractr", quiet = TRUE)) == 0) stop(
+    "exactextractr package required. Install with `install.packages(\"exactextractr\")`")
+
+  # Crop and reproject
+  reproject <- !terra::compareGeom(
+    terra::rast(crs = terra::crs(input)), templateRast,
+    crs = TRUE, warncrs = FALSE, stopOnError = FALSE, messages = FALSE,
+    lyrs = FALSE, ext = FALSE, rowcol = FALSE, res = FALSE)
+
+  cropBBOX <- sf::st_buffer(
+    sf::st_as_sfc(sf::st_bbox(templateRast)), terra::res(templateRast)[[1]],
+    joinStyle = "MITRE", mitreLimit = 2)
+
+  .muffleWarningAgr <- function(w){
+    agrWarning <- "attribute variables are assumed to be spatially constant throughout all geometries"
+    if (w$message == agrWarning) invokeRestart("muffleWarning")
+  }
+
+  if (reproject){
+
+    input <- withCallingHandlers(
+      sf::st_intersection(input, sf::st_transform(cropBBOX, sf::st_crs(input))),
+      warning = .muffleWarningAgr)
+    sf::st_geometry(input) <- sf::st_transform(sf::st_geometry(input), sf::st_crs(templateRast))
+
+  }else{
+
+    input <- withCallingHandlers(
+      sf::st_crop(input, cropBBOX),
+      warning = .muffleWarningAgr)
+  }
+
+  # Rasterize
+  cellIdxRast <- exactextractr::rasterize_polygons(
+    input, templateRast, min_coverage = 0.5)
+
+  # Return values
+  input[[field]][terra::values(cellIdxRast)]
+}
+
+
+#' Write raster with values
+#'
+#' Replace the values in a `SpatRaster` before writing it to file.
+#'
+#' @param templateRast terra `SpatRaster` template defining raster geometry.
+#' @param values numeric or character. New raster values.
+#' @param filename character. Output filename.
+#' @param ... arguments to \code{\link[terra]{writeRaster}}
+#'
+#' @export
+writeRasterWithValues <- function(templateRast, values, filename, ...){
+
+  if (!is.numeric(values)) values <- factor(values)
+  terra::values(templateRast) <- values
+
+  dir.create(dirname(filename), recursive = TRUE, showWarnings = FALSE)
+  terra::writeRaster(templateRast, filename = filename, ...)
+
+  return(invisible())
+}
+
