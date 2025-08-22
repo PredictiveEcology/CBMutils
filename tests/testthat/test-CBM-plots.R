67137524daa0a83433661826bddf19c84b5bab97
Susan Murray <murray.e.susan@gmail.com>
2025-08-22 07:52:29 -0700

    NPPplot test fix

--- tests/testthat/test-CBM-plots.R
+++ tests/testthat/test-CBM-plots.R
@@ -2,6 +2,13 @@
 if (!testthat::is_testing()) source(testthat::test_path("setup.R"))
 
 masterRaster <- terra::rast(matrix(rep(1,4), nrow = 2, ncol = 2))
+
+cohortGroupKeep <- data.table::data.table(
+  pixelIndex = c(1:4),
+  cohortGroupID = c(1:4),
+  `1` = c(4:1),
+  `2` = c(1:4)
+)
 cbmPoolsTest <- data.table::data.table(
   simYear = c(rep(1,4), rep(2,4)),
   cohortGroupID = rep(c(1:4),2),
@@ -9,11 +16,10 @@ cbmPoolsTest <- data.table::data.table(
   Merch = 1:8,
   BranchSnag = 101:108
 )
-cohortGroupKeep <- data.table::data.table(
-  pixelIndex = c(1:4),
-  cohortGroupID = c(1:4),
-  `1` = c(4:1),
-  `2` = c(1:4)
+NPPTest <- data.table::data.table(
+  simYear = rep(c(1:2), each = 4),
+  cohortGroupID = rep(c(1:4), 2),
+  NPP = c(c(1:4), c(4:1))
 )
 
 test_that("spatialPlot", {
@@ -29,17 +35,15 @@ test_that("carbonOutPlot", {
 
 })
 
-NPPTest <- data.table::data.table(
-  simYear = rep(c(1:2), each = 4),
-  cohortGroupID = rep(c(1:4), 2),
-  NPP = c(c(1:4), c(4:1))
-)
-
 test_that("NPPplot", {
-  out <- NPPplot(cohortGroupKeep, NPPTest, masterRaster)
+  out <- NPPplot(
+    NPP = NPPTest, year = 1,
+    cohortGroupKeep = cohortGroupKeep,
+    masterRaster = masterRaster
+  )
   expect_is(out, "ggplot")
   expect_equal(out$plot_env$overallAvgNpp, 2.5)
-  expect_equal(out$plot_env$plotMaster$avgNPP, c(4, 3, 2, 1))
+  expect_equal(out$plot_env$plotMaster$NPP, c(4, 3, 2, 1))
 })
 
 test_that("barPlot", {
