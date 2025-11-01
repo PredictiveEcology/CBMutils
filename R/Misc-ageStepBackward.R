e842896d33100ed1bdfde85612449eb3b5b81652
Susan Murray <murray.e.susan@gmail.com>
2025-11-01 11:22:59 -0700

    ageStepBackward parameter update

--- R/Misc-ageStepBackward.R
+++ R/Misc-ageStepBackward.R
@@ -13,6 +13,7 @@ utils::globalVariables(c("x", "y", "z"))
 #' Table of disturbance events with columns "pixelIndex" and "year".
 #' If provided, disturbances will be reversed and the disturbed areas
 #' will be filled with `gstat_replace`.
+#' @param ... arguments to \code{\link{gstat_replace}}.
 #' @inheritParams gstat_replace
 #'
 #' @return \code{SpatRaster}
@@ -20,7 +21,7 @@ utils::globalVariables(c("x", "y", "z"))
 #' @export
 ageStepBackward <- function(
     ageRast, yearIn, yearOut, fill = TRUE, distEvents = NULL,
-    idw = TRUE, formula = z~1, agg.fact = 2, agg.fun = "median", ...){
+    idp = 2, nmax = 250, ...){
 
   if (yearIn == yearOut) return(ageRast)
   if (yearIn <  yearOut) stop("Year input is less than year output. Use `ageStepForward`")
@@ -82,13 +83,10 @@ ageStepBackward <- function(
       # Reverse disturbances and fill
       if (length(distCells) > 0){
         ageRast <- gstat_replace(
-          inRast   = ageRast,
-          cells    = distCells,
-          ignore   = -500:0,
-          idw      = idw,
-          formula  = formula,
-          agg.fact = agg.fact,
-          agg.fun  = agg.fun,
+          ageRast, distCells,
+          ignore = -500:0,
+          idp    = idp,
+          nmax   = nmax,
           ...)
       }
 
@@ -108,12 +106,9 @@ ageStepBackward <- function(
       message("Replacing ages <=0 in ", length(cellsLTE0), " pixels")
 
       ageRast <- gstat_replace(
-        inRast   = ageRast,
-        cells    = cellsLTE0,
-        idw      = idw,
-        formula  = formula,
-        agg.fact = agg.fact,
-        agg.fun  = agg.fun,
+        ageRast, cellsLTE0,
+        idp    = idp,
+        nmax   = nmax,
         ...)
 
     }else{
@@ -135,21 +130,24 @@ ageStepBackward <- function(
 #'
 #' @param ignore numeric. Raster values to exclude from input data.
 #' @param idw logical. Use \code{\link[gstat]{idw}} or \code{\link[gstat]{krige}}.
-#' @param formula Interpolation formula.
+#' @param idp numeric. IDW power.
+#' @param nmax numeric. The maximum number of nearest observations to use.
+#' @param maxdist numeric. The maximum distance of observations to use.
 #' @param agg.fact Aggregation factor for the input raster.
 #' If >1, the raster will be aggregated before cell values
 #' are extracted as input data points for interpolation.
 #' This smooths the input data and speeds up the interpolation process.
 #' See \code{\link[terra]{aggregate}}.
 #' @param agg.fun Aggregation function.
-#' @param agg.na.rm Remove NA cells in aggregation function.
+#' @param agg.na.rm Remove NA cells when aggregating.
 #' @param ... additional arguments to \code{\link[gstat]{idw}} or \code{\link[gstat]{krige}}.
 #'
 #' @keywords internal
 gstat_replace <- function(
     inRast, cells, ignore = NULL,
-    idw = TRUE, formula = z~1, ...,
-    agg.fact = 2, agg.fun = "median", agg.na.rm = TRUE,
+    idw = TRUE, idp = 2,
+    nmax = Inf, maxdist = Inf, ...,
+    agg.fact = 1, agg.fun = "median", agg.na.rm = TRUE,
     verbose = TRUE){
 
   if (length(cells) == 0){
@@ -165,35 +163,34 @@ gstat_replace <- function(
 
     if (verbose) message("gstat_replace: Aggregating input raster")
 
-    if (!is.null(ignore)){
-      smRast <- terra::classify(smRast, cbind(ignore, NA))
-    }
+    if (!is.null(ignore)) smRast <- terra::classify(smRast, cbind(ignore, NA))
     smRast <- terra::aggregate(smRast, fact = agg.fact, fun = agg.fun, na.rm = agg.na.rm)
   }
 
   if (verbose) message("gstat_replace: Predicting new values")
 
-  inData <- terra::extract(smRast, terra::cells(smRast), xy = TRUE)
-  names(inData)[[3]] <- "z"
-  if (!is.null(ignore) & !agg.fact > 1){
-    inData <- subset(inData, !z %in% ignore)
-  }
-
-  if (nrow(inData) == 0) stop("Raster does not contain any values to use as predictors")
-
-  gstatFunc <- if (idw) gstat::idw else gstat::krige
-  newVals <- gstatFunc(
-    formula     = formula,
-    locations   = ~x+y,
-    data        = inData,
-    newdata     = as.data.frame(terra::xyFromCell(inRast, cells)),
+  inArgs  <- list(
     debug.level = 0,
-    ...
-  )[, 3]
-  rm(inData)
+    locations = ~x+y,
+    data      = data.table::data.table(terra::extract(smRast, terra::cells(smRast), xy = TRUE)),
+    newdata   = data.table::data.table(terra::xyFromCell(inRast, cells)),
+    formula   = if (idw) z~1,
+    idp       = if (idw) idp,
+    nmax      = nmax,
+    maxdist   = maxdist
+  )
+  data.table::setnames(inArgs$data, names(inArgs$data)[[3]], "z")
+  if (!is.null(ignore) & agg.fact == 1) inArgs$data <- inArgs$data[!z %in% ignore,]
+  if (nrow(inArgs$data) == 0) stop("Raster does not contain any values to use as predictors")
 
-  if (any(is.na(newVals))) stop(
-    "gstat failed to predict all replacement values. Try adjusting parameters")
+  dotArgs <- list(...)
+  inArgs <- c(inArgs[!sapply(inArgs, is.null) & !names(inArgs) %in% names(dotArgs)], dotArgs)
+  rm(dotArgs)
+
+  newVals <- do.call(if (idw) gstat::idw else gstat::krige, inArgs)[,3]
+  rm(inArgs)
+
+  if (any(is.na(newVals))) stop("gstat failed to predict all replacement values. Try adjusting parameters")
 
   terra::set.values(inRast, cells, newVals)
 
