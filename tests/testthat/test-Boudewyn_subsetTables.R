d01d7badb9ab996d1e6eed14da1aaa252a4998aa
Susan Murray <murray.e.susan@gmail.com>
2025-11-06 08:18:49 -0800

    boudewynSubsetTables tests updated to test every spatial_unit_id; temporarily limited to just BC and SK

--- tests/testthat/test-Boudewyn_subsetTables.R
+++ tests/testthat/test-Boudewyn_subsetTables.R
@@ -1,22 +1,55 @@
 if (!testthat::is_testing()) source(testthat::test_path("setup.R"))
 
-table3 <- reproducible::prepInputs(url = "https://nfi.nfis.org/resources/biomass_models/appendix2_table3.csv",
-                                      fun = "data.table::fread",
-                                      destinationPath = testDirs$temp$inputs,
-                                      filename2 = "appendix2_table3.csv")
-eco <- c("9")
+# Download CBM-CFS3 database
+dbPath <- {
+  url = "https://raw.githubusercontent.com/cat-cfs/libcbm_py/main/libcbm/resources/cbm_defaults_db/cbm_defaults_v1.2.8340.362.db"
+  destfile <- file.path(testDirs$temp$inputs, basename(url))
+  if (!file.exists(destfile)) download.file(url = url, destfile = destfile, mode = "wb", quiet = TRUE)
+  destfile
+}
 
-thisAdmin <- data.frame(
-  AdminBoundaryID = 9,
-  stump_parameter_id = 9,
-  adminName = "Saskatchewan",
-  abreviation = "SK",
-  SpatialUnitID = 28,
-  EcoBoundaryID = 9
-)
-
-test_that("boudewynSubsetTables", {
-  out <- boudewynSubsetTables(table = table3, thisAdmin = thisAdmin, eco = eco)
-
-  expect_is(out, "data.table")
+# Download Boudewyn tables
+boudewynTables <- lapply(3:7, function(n){
+  url <- paste0("https://nfi.nfis.org/resources/biomass_models/appendix2_table", n, ".csv")
+  destfile <- file.path(testDirs$temp$inputs, basename(url))
+  if (!file.exists(destfile)) download.file(url = url, destfile = destfile, mode = "wb", quiet = TRUE)
+  data.table::fread(destfile)
 })
+names(boudewynTables) <- 3:7
+
+
+cbmDBcon <- DBI::dbConnect(RSQLite::dbDriver("SQLite"), dbPath)
+spatial_units <- RSQLite::dbReadTable(cbmDBcon, "spatial_unit") |>
+  data.table::as.data.table()
+
+spatial_units[, AdminBoundaryID := admin_boundary_id]
+spatial_units[, EcoBoundaryID   := eco_boundary_id]
+
+data.table::setkey(spatial_units, admin_boundary_id, eco_boundary_id)
+
+## TEMPORARY: test only a subset of provinces
+spatial_units <- spatial_units[admin_boundary_id %in% c(
+  9, # SK
+  11 # BC
+),]
+
+for (i in 1:nrow(spatial_units)){
+
+  testName <- with(spatial_units[i,], sprintf(
+    "boudewynSubsetTables: admin_boundary_id = %s; eco_boundary_id = %s; spatial_unit_id = %s; ",
+    admin_boundary_id, eco_boundary_id, id))
+
+  test_that(testName, {
+
+    thisAdmin <- spatial_units[i,]
+    eco <- thisAdmin$EcoBoundaryID
+
+    expect_is(boudewynSubsetTables(table = boudewynTables[["3"]], thisAdmin = thisAdmin, eco = eco), "data.table")
+    expect_is(boudewynSubsetTables(table = boudewynTables[["4"]], thisAdmin = thisAdmin, eco = eco), "data.table")
+    expect_is(boudewynSubsetTables(table = boudewynTables[["5"]], thisAdmin = thisAdmin, eco = eco), "data.table")
+    expect_is(boudewynSubsetTables(table = boudewynTables[["6"]], thisAdmin = thisAdmin, eco = eco), "data.table")
+    expect_is(boudewynSubsetTables(table = boudewynTables[["7"]], thisAdmin = thisAdmin, eco = eco), "data.table")
+  })
+}
+
+
