f887d90da2539f66aa1d4ba6d2fec4aee4943999
Susan Murray <murray.e.susan@gmail.com>
2026-09-05 10:28:42 -0700

    tests read Boudewyn parameters in setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,3 +6,17 @@ if (!testthat::is_testing()){
 
 # Set up test directories
 testDirs <- .testDirectorySetUp()
+
+# Read Boudewyn parameters
+bParams <- lapply(
+  list(
+    table3   = "appendix2_table3.csv",
+    table4   = "appendix2_table4.csv",
+    table5   = "appendix2_table5.csv",
+    table6   = "appendix2_table6.csv",
+    table6tb = "appendix2_table6_tb.csv",
+    table7   = "appendix2_table7.csv",
+    table7tb = "appendix2_table7_tb.csv"
+  ),
+  function(f) data.table::fread(file.path("https://nfi.nfis.org/resources/biomass_models", f))
+)
